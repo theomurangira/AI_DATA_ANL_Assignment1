9a7678148a04a883a6ba4975bc6feb478f276882
Theo Murangira <theoneste@yahoo.com>
2024-11-13 16:34:09 +0200

    Assignments created

---
+++ Assignment1.py
@@ -0,0 +1,53 @@
+""" Consider the lists countries and cities
+"""
+countries=["Rwanda","DRC","CAR","Gabon","Congo"]
+cities=["Kigali","Kinshasa","Bangui","Libreville","Brazzaville"]
+""" Use a loop to create a list of tuples, where each tuple 
+represents a country and  its capital city """
+###your code starts here
+
+
+
+
+### your code ends here
+
+""" Create the same list of tuples but using the zip() function
+"""
+
+###Your code start here
+
+
+
+##your code ends here
+
+""" Change the list of tuples you have created above into 
+a list of lists
+"""
+###your code starts here
+
+
+
+###your code ends here
+
+""" The following lists 
+"""
+countries_1=["Germany","France","Belgium","India"]*2
+cities_1=["Frankfurt","Paris","Liege","Bangalore","Bonn","Bordeaux","Brussels","Dehli"]
+
+""" Use a loop to create a dictionary where, the key is
+ a country and the value is the list of cities in that country"""
+
+### Your code starts here
+
+
+
+###your code ends here
+
+""" Do the same task as above , but this time use dictionary comprehension
+"""
+### Your code starts here
+
+
+####Your code ends here
+
+
